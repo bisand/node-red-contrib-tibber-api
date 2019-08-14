e513dd42af5c7e9dbcef0981a96f1a8306e877d7
André Biseth <andre@biseth.net>
2019-08-14 16:27:19 +0200

    Added more config and logic

---
+++ TibberFeed.js
@@ -0,0 +1,116 @@
+const WebSocket = require('ws');
+var events = require('events');
+var ws = null;
+
+class TibberFeed {
+    constructor(config) {
+        if (!ws)
+            ws = new WebSocket(config.apiUrl, ['graphql-ws']);
+        console.log('Created!');
+        var self = this;
+        self.apikey = config.apikey;
+        self.homeid = config.homeid;
+
+        var gql = 'subscription{\nliveMeasurement(homeId:\"' + self.homeid + '\"){\n';
+        if (config.timestamp == 1)
+            gql += 'timestamp\n';
+        if (config.power == 1)
+            gql += 'power\n';
+        if (config.lastMeterConsumption == 1)
+            gql += 'lastMeterConsumption\n';
+        if (config.accumulatedConsumption == 1)
+            gql += 'accumulatedConsumption\n';
+        if (config.accumulatedProduction == 1)
+            gql += 'accumulatedProduction\n';
+        if (config.accumulatedCost == 1)
+            gql += 'accumulatedCost\n';
+        if (config.accumulatedReward == 1)
+            gql += 'accumulatedReward\n';
+        if (config.currency == 1)
+            gql += 'currency\n';
+        if (config.minPower == 1)
+            gql += 'minPower\n';
+        if (config.averagePower == 1)
+            gql += 'averagePower\n';
+        if (config.maxPower == 1)
+            gql += 'maxPower\n';
+        if (config.powerProduction == 1)
+            gql += 'powerProduction\n';
+        if (config.minPowerProduction == 1)
+            gql += 'minPowerProduction\n';
+        if (config.maxPowerProduction == 1)
+            gql += 'maxPowerProduction\n';
+        if (config.lastMeterProduction == 1)
+            gql += 'lastMeterProduction\n';
+        if (config.powerFactor == 1)
+            gql += 'powerFactor\n';
+        if (config.voltagePhase1 == 1)
+            gql += 'voltagePhase1\n';
+        if (config.voltagePhase2 == 1)
+            gql += 'voltagePhase2\n';
+        if (config.voltagePhase3 == 1)
+            gql += 'voltagePhase3\n';
+        if (config.currentPhase1 == 1)
+            gql += 'currentPhase1\n';
+        if (config.currentPhase2 == 1)
+            gql += 'currentPhase2\n';
+        if (config.currentPhase3 == 1)
+            gql += 'currentPhase3\n';
+        gql += '}}';
+        self.query = {
+            id: "1",
+            type: "start",
+            payload: {
+                variables: {},
+                extensions: {},
+                operationName: null,
+                query: gql
+            }
+        };
+
+        self.events = new events.EventEmitter();
+
+        ws.on('open', function () {
+            console.log('Connected!');
+            ws.send('{"type":"connection_init","payload":"token=' + self.apikey + '"}')
+            self.events.emit('connected', "Connected to Tibber feed");
+        });
+
+        ws.on('message', function (message) {
+            if (message.startsWith('{')) {
+                var msg = JSON.parse(message);
+                if (msg.type == 'connection_ack') {
+                    self.events.emit('connection_ack', msg);
+                    var str = JSON.stringify(self.query);
+                    ws.send(str);
+                }
+                else if (msg.type == "connection_error") {
+                    self.events.emit('error', msg);
+                }
+                else if (msg.type == "data") {
+                    if (!msg.payload.data)
+                        return;
+                    var data = msg.payload.data.liveMeasurement;
+                    self.events.emit('data', data);
+                }
+            }
+        });
+
+        ws.on('close', function (code) {
+            console.log('Disconnected: ' + code);
+            self.events.emit('disconnected', "Disconnected from Tibber feed");
+        });
+
+        ws.on('error', function (error) {
+            console.log('Error: ' + error.code);
+            self.events.emit('error', error);
+        });
+
+        self.close = function(){
+            ws.close();
+            ws.terminate();
+            ws = null;
+        };
+    }
+}
+module.exports = TibberFeed;
--- tibber-api.js
+++ tibber-api.js
@@ -1,26 +1,46 @@
-
+var TibberFeed = require('./TibberFeed');
 
 module.exports = function (RED) {
     function tibberApiNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
 
-        var intervalId = setInterval(myTimer, 1000);
+        if( !config.apiUrl || !config.apikey || !config.homeid){
+            this.error('Missing mandatory parameters');
+            return;
+        }
+        var tibberFeed = new TibberFeed(config)
+        tibberFeed.events.on('data', function (data) {
+            var msg = { payload: data };
+            node.send(msg);
+        });
+
+        tibberFeed.events.on('connected', function (data) {
+            var msg = { payload: data };
+            node.send(msg);
+        });
+
+        tibberFeed.events.on('connection_ack', function (data) {
+            var msg = { payload: data };
+            node.send(msg);
+        });
+
+        tibberFeed.events.on('disconnected', function (data) {
+            var msg = { payload: data };
+            node.send(msg);
+        });
+
+        tibberFeed.events.on('error', function (data) {
+            var msg = { payload: data };
+            node.send(msg);
+        });
 
         node.on('input', function (msg) {
-            msg.payload = { apikey: config.apikey, homeid: config.homeid };
             node.send(msg);
         });
         node.on('close', function () {
-            clearInterval(intervalId);
+            tibberFeed.close();
         });
-
-        function myTimer() {
-            var d = new Date();
-            var t = d.toLocaleTimeString();
-            var msg = { payload: { date: d, time: t } };
-            node.send(msg)
-        }
     }
-    RED.nodes.registerType("tibber-api", tibberApiNode);
+    RED.nodes.registerType("tibber-feed", tibberApiNode);
 }
