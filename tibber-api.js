e4fe150a0ee6fd5befc22b1c569ab804205bc97f
André Biseth <andre@biseth.net>
2019-08-14 11:16:21 +0200

    Figuring out logic

--- tibber-api.js
+++ tibber-api.js
@@ -3,13 +3,24 @@
 module.exports = function (RED) {
     function tibberApiNode(config) {
         RED.nodes.createNode(this, config);
-        var msg = { payload: { test: "Started Tibber Test" } };
         var node = this;
-        node.send(msg);
+
+        var intervalId = setInterval(myTimer, 1000);
+
         node.on('input', function (msg) {
             msg.payload = { apikey: config.apikey, homeid: config.homeid };
             node.send(msg);
         });
+        node.on('close', function () {
+            clearInterval(intervalId);
+        });
+
+        function myTimer() {
+            var d = new Date();
+            var t = d.toLocaleTimeString();
+            var msg = { payload: { time: t } };
+            node.send(msg)
+        }
     }
     RED.nodes.registerType("tibber-api", tibberApiNode);
 }
