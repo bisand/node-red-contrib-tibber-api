38e09fea5351dc07bcf58811db3d87d15b758044
André Biseth <andre@biseth.net>
2019-09-30 07:13:50 +0200

    Modified failing tests.

--- tests/test-tibber-api-endpoint.js
+++ tests/test-tibber-api-endpoint.js
@@ -1,22 +1,24 @@
 /* eslint-env mocha */
-let urlValidator = require('tibber-api').urlValidator;
+let UrlTools = require('tibber-api').UrlTools;
 let assert = require('assert');
+const urlTools = new UrlTools();
+
 describe('tibber-api-endpoint', function () {
   describe('Validate Url', function () {
     it('Websocket url should be valid', function () {
-      let isValid = urlValidator("wss://api.tibber.com/v1-beta/gql/subscriptions");
+      let isValid = urlTools.validateUrl("wss://api.tibber.com/v1-beta/gql/subscriptions");
       assert.ok(isValid);
     });
     it('Query url should be valid', function () {
-      let isValid = urlValidator("https://api.tibber.com/v1-beta/gql");
+      let isValid = urlTools.validateUrl("https://api.tibber.com/v1-beta/gql");
       assert.ok(isValid);
     });
     it('Websocket url should be invalid', function () {
-      let isValid = urlValidator("wss//api.tibber.com/v1-beta/gql/subscriptions");
+      let isValid = urlTools.validateUrl("wss//api.tibber.com/v1-beta/gql/subscriptions");
       assert.ok(!isValid);
     });
     it('Query url should be invalid', function () {
-      let isValid = urlValidator("https//api.tibber.com/v1-beta/gql");
+      let isValid = urlTools.validateUrl("https//api.tibber.com/v1-beta/gql");
       assert.ok(!isValid);
     });
   });
