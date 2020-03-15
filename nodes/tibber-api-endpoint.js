6c5593dd8a04c111c6e26e4567dfc8d3589310d8
André Biseth <andre@biseth.net>
2020-03-15 18:15:50 +0100

    Working to get credentials to work with prev apikey

--- nodes/tibber-api-endpoint.js
+++ nodes/tibber-api-endpoint.js
@@ -1,9 +1,23 @@
-module.exports = function (RED) {
-  function TibberApiEndpointNode(config) {
-    RED.nodes.createNode(this, config);
-    this.feedUrl = config.feedUrl;
-    this.queryUrl = config.queryUrl;
-    this.apiKey = config.apiKey;
-  }
-  RED.nodes.registerType("tibber-api-endpoint", TibberApiEndpointNode);
+module.exports = function(RED) {
+    function TibberApiEndpointNode(config) {
+        RED.nodes.createNode(this, config);
+        this.feedUrl = config.feedUrl;
+        this.queryUrl = config.queryUrl;
+        this.apiKey = config.apiKey;
+
+        console.log(this.credentials);
+        console.log(config);
+
+        if (this.credentials && !this.credentials.secureApiKey && config.apiKey) {
+            RED.nodes.addCredentials(this.id, { secureApiKey: config.apiKey });
+            this.apiKey = config.apiKey = '';
+        } else {
+            this.credentials.secureApiKey = 'Test 123';
+        }
+    }
+    RED.nodes.registerType('tibber-api-endpoint', TibberApiEndpointNode, {
+        credentials: {
+            secureApiKey: { type: 'text', required: true },
+        },
+    });
 };
