0d1463f8aa7c21c4d9efba011b7eedb8bd1e7c99
André Biseth <andre@biseth.net>
2021-03-23 13:28:49 +0100

    Improved concurrency and cleaned up code

--- nodes/tibber-feed.js
+++ nodes/tibber-feed.js
@@ -4,86 +4,92 @@ module.exports = function (RED) {
     function TibberFeedNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+        var _config = config;
         node.status({ fill: "red", shape: "ring", text: "disconnected" });
 
-        config.apiEndpoint = RED.nodes.getNode(config.apiEndpointRef);
+        _config.apiEndpoint = RED.nodes.getNode(_config.apiEndpointRef);
 
-        var credentials = RED.nodes.getCredentials(config.apiEndpointRef);
-        if (!config.apiEndpoint.feedUrl || !credentials || !credentials.accessToken || !config.homeId) {
+        var credentials = RED.nodes.getCredentials(_config.apiEndpointRef);
+        if (!_config.apiEndpoint.feedUrl || !credentials || !credentials.accessToken || !_config.homeId) {
             node.error('Missing mandatory parameters. Execution will halt. Please reconfigure and publish again.');
             return;
         }
 
-        // Assign access token to api key to meintain compatibility. This will not cause the access token to be exported.
-        config.apiEndpoint.apiKey = credentials.accessToken;
-
-        if (config.active && !TibberFeedNode.instances[config.apiEndpoint.apiKey])
-            TibberFeedNode.instances[config.apiEndpoint.apiKey] = new TibberFeed(config);
-
-        if (!config.active) {
-            node.status({ fill: "red", shape: "ring", text: "disconnected" });
-            if (!TibberFeedNode.instances[config.apiEndpoint.apiKey])
-                return;
-            TibberFeedNode.instances[config.apiEndpoint.apiKey].close();
-            TibberFeedNode.instances[config.apiEndpoint.apiKey].active = false;
-            delete TibberFeedNode.instances[config.apiEndpoint.apiKey];
-
+        if (!_config.active) {
             return;
         }
 
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('data', function (data) {
+        // Assign access token to api key to meintain compatibility. This will not cause the access token to be exported.
+        _config.apiEndpoint.apiKey = credentials.accessToken;
+
+        if (!TibberFeedNode.instances[_config.apiEndpoint.apiKey]) {
+            TibberFeedNode.instances[_config.apiEndpoint.apiKey] = new TibberFeed(config);
+        }
+        node._feed = TibberFeedNode.instances[_config.apiEndpoint.apiKey];
+
+        node.listeners = {};
+        node.listeners.onDataReceived = function onDataReceived(data) {
             var msg = {
                 payload: data
             };
-            if (TibberFeedNode.instances[config.apiEndpoint.apiKey].connected) {
+            if (_config.active && node._feed.connected) {
                 node.status({ fill: "green", shape: "dot", text: "connected" });
+                node.send(msg);
+                node._feed.heartbeat();
             } else {
                 node.status({ fill: "red", shape: "ring", text: "disconnected" });
             }
-            node.send(msg);
-            if (TibberFeedNode.instances[config.apiEndpoint.apiKey])
-                TibberFeedNode.instances[config.apiEndpoint.apiKey].heartbeat();
-        });
-
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('connected', function (data) {
+        };
+        node.listeners.onConnected = function onConnected(data) {
             node.status({ fill: "green", shape: "dot", text: "connected" });
             node.log(data);
-        });
-
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('connection_ack', function (data) {
-            node.status({ fill: "green", shape: "dot", text: "connected" });
-            node.log(data);
-        });
-
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('disconnected', function (data) {
+        };
+        node.listeners.onDisconnected = function onDisconnected(data) {
             node.status({ fill: "red", shape: "ring", text: "disconnected" });
             node.log(data);
-            if (TibberFeedNode.instances[config.apiEndpoint.apiKey])
-                TibberFeedNode.instances[config.apiEndpoint.apiKey].heartbeat();
-        });
-
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('error', function (data) {
+            node._feed.heartbeat();
+        };
+        node.listeners.onError = function onError(data) {
             node.error(data);
-        });
-
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('warn', function (data) {
+        };
+        node.listeners.onWarn = function onWarn(data) {
             node.warn(data);
-        });
-
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].on('log', function (data) {
+        };
+        node.listeners.onLog = function onLog(data) {
             node.log(data);
-        });
+        };
 
+        if (_config.active) {
+            node._feed.on('data', node.listeners.onDataReceived);
+            node._feed.on('connected', node.listeners.onConnected);
+            node._feed.on('connection_ack', node.listeners.onConnected);
+            node._feed.on('disconnected', node.listeners.onDisconnected);
+            node._feed.on('error', node.listeners.onError);
+            node._feed.on('warn', node.listeners.onWarn);
+            node._feed.on('log', node.listeners.onLog);
+        }
         node.on('close', function () {
             node.status({ fill: "red", shape: "ring", text: "disconnected" });
-            if (!TibberFeedNode.instances[config.apiEndpoint.apiKey])
+            if (!node._feed)
                 return;
-            TibberFeedNode.instances[config.apiEndpoint.apiKey].close();
+            node._feed.off('data', node.listeners.onDataReceived);
+            node._feed.off('connected', node.listeners.onConnected);
+            node._feed.off('connection_ack', node.listeners.onConnected);
+            node._feed.off('disconnected', node.listeners.onDisconnected);
+            node._feed.off('error', node.listeners.onError);
+            node._feed.off('warn', node.listeners.onWarn);
+            node._feed.off('log', node.listeners.onLog);
+            node._feed = null;
+            node.listeners = null;
         });
 
-        TibberFeedNode.instances[config.apiEndpoint.apiKey].connect();
+        if (!node._feed.connected) {
+            node._feed.connect();
+        }
+
     }
     TibberFeedNode.instances = {};
+    TibberFeedNode.functions = {};
 
     RED.nodes.registerType("tibber-feed", TibberFeedNode);
 };
