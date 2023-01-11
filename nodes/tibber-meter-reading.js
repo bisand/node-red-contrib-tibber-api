af76204e092a3d521dd146f6d3590247600b9c7a
André Biseth <andre@biseth.net>
2023-01-11 12:37:04 +0000

    Add meter reading mutation node

---
+++ nodes/tibber-meter-reading.js
@@ -0,0 +1,58 @@
+const TibberQuery = require("tibber-api").TibberQuery;
+
+module.exports = function (RED) {
+  function TibberMeeterReadingNode(config) {
+    RED.nodes.createNode(this, config);
+    config.apiEndpoint = RED.nodes.getNode(config.apiEndpointRef);
+    this._config = config;
+
+    var credentials = RED.nodes.getCredentials(config.apiEndpointRef);
+    if (!config.apiEndpoint.queryUrl || !credentials || !credentials.accessToken) {
+      this.error("Missing mandatory parameters (queryUrl and/or accessToken)");
+      return;
+    }
+
+    // Assign access token to api key to meintain compatibility. This will not cause the access token to be exported.
+    config.apiEndpoint.apiKey = credentials.accessToken;
+    this.client = new TibberQuery(config);
+
+    this.on("input", async (msg) => {
+      var homeId = this._config.homeId
+        ? this._config.homeId
+        : msg.payload.title;
+      var time = this._config.time
+        ? this._config.time
+        : msg.payload.message;
+      var reading = this._config.reading
+        ? this._config.reading
+        : msg.payload.screen;
+      reading = reading ? reading : "HOME";
+
+      if (!homeId || !time) {
+        this.error("Missing mandatory parameters (title and/or message)");
+        return;
+      }
+
+      var query =
+        'mutation{sendMeterReading(input: {homeId: "' +
+        homeId +
+        '", time: "' +
+        time +
+        '", reading: ' +
+        reading +
+        "}){homeId time reading}}";
+      try {
+        var result = await this.client.query(query);
+        if (result.error) {
+          this.error(JSON.stringify(result));
+        } else {
+          this.log(JSON.stringify(result));
+        }
+      } catch (error) {
+        this.error(error);
+      }
+    });
+  }
+
+  RED.nodes.registerType("tibber-meter-reading", TibberMeeterReadingNode);
+};
