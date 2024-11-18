55cb5a07b760bd75a4d5064c4ced973c4a0b5f8b
André Biseth <andre@biseth.net>
2024-11-18 11:36:30 +0000

    Removed version

--- Version.ts
+++
@@ -1,9 +0,0 @@
-export const version = "6.2.0";
-export const versionMajor = 6;
-export const versionMinor = 2;
-export const versionPatch = 0;
-export const versionPrerelease = [];
-export const release = "6.2.0+20241118113536";
-export const gitSha = "efae03951aac35602e7c835595c9881548d388e4";
-export const gitDate = new Date(1731929736000);
-export default {version,versionMajor,versionMinor,versionPatch,versionPrerelease,release,gitSha,gitDate};
